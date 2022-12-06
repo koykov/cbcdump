78cde11bc5ca07af2428047b48a861949ef6a657
Konstantin Voykov <koykov0@gmail.com>
2022-12-06 21:48:05 +0200

    Cover FS reader with tests.

---
+++ fs/reader_test.go
@@ -0,0 +1,46 @@
+package fs
+
+import (
+	"bytes"
+	"io"
+	"math"
+	"strconv"
+	"testing"
+
+	"github.com/koykov/cbytecache"
+)
+
+func TestReader(t *testing.T) {
+	r := Reader{
+		FilePath: "testdata/example.bin",
+		OnEOF:    func(_ string) error { return nil }, // dummy EOF function to keep testing dump file
+	}
+
+	for {
+		e, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if !assertEntry(e) {
+			t.FailNow()
+		}
+	}
+}
+
+func assertEntry(e cbytecache.Entry) bool {
+	if len(e.Key) < 3 {
+		return false
+	}
+	n, err := strconv.Atoi(e.Key[3:])
+	if err != nil {
+		return false
+	}
+	exp := getTestBody(n)
+	if !bytes.Equal(exp, e.Body) {
+		return false
+	}
+	if e.Expire != math.MaxUint32 {
+		return false
+	}
+	return true
+}
